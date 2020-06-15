cc40df15d3a37cfc34d05bd9f0be98bac9b06324
vipin <vipinvincentp13@gmail.comm>
2020-06-15 18:39:07 +0530

    ga changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { name } from '../package.json'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   render() {
@@ -50,6 +51,23 @@ export default class MyDocument extends Document {
           <link
             href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap'
             rel='stylesheet'
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
           />
         </Head>
         <body id='main'>
