951edf907b7548488a579bd812a6ed1cdffb306f
vipin <vipinvincentp13@gmail.comm>
2020-07-05 12:02:53 +0530

    austrian gp qualifying  deployment

--- pages/qualifying-results.tsx
+++ pages/qualifying-results.tsx
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <div className='w-auto flex flex-col items-center content-center justify-center '>
       <div className='w-full h-full rounded text-center text-black-70 my-2 sm:my-4 px-2 sm:px-4 pb-0'>
         <div className='flex flex-row'>
-          <div className='flex flex-col lg:w-2/3'>
+          <div className='flex flex-col w-full lg:w-2/3'>
             <Qualifying />
           </div>
           <div className='hidden lg:block w-1/3 pl-4'>
