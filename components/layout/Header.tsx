9f3772d4ebb0989e8bc05fa3af2568c3c76dd7d2
vipin <vipinvincentp13@gmail.comm>
2020-07-06 16:18:08 +0530

    UI updates

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import NavLink from 'components/link/NavLink'
-import TopNav from 'components/link/TopNav'
 import { motion } from 'framer-motion'
 
-const Home = dynamic(() => import('components/icons/latest'))
+const News = dynamic(() => import('components/icons/news'))
 const Team = dynamic(() => import('components/icons/team'))
 const Driver = dynamic(() => import('components/icons/driver-alt'))
 const Circuit = dynamic(() => import('components/icons/circuit-alt'))
@@ -16,62 +15,62 @@ const Header: React.FC = () => {
       className='w-full bg-white-100 lg:flex lg:items-center lg:justify-between mx-auto py-1 header container z-20'
       id='top'>
       <div className='hidden lg:flex justify-end lg:order-2 w-8/12 pr-4 h-20 whitespace-no-wrap'>
-        <div className='h-20 pl-2 pr-4 flex flex-row content-center self-center grow'>
-          <div className='flex content-center self-center leading-tight text-base'>
-            <Home />
+        <NavLink href='/' target='_self'>
+          <div className='h-20 pl-2 pr-4 flex flex-row content-center self-center grow'>
+            <div className='flex content-center self-center leading-tight text-base'>
+              <News />
+            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.1, duration: 1 }}
+              className='flex content-center self-center leading-tight text-base pt-0'>
+              Latest News
+            </motion.div>
           </div>
-          <motion.div
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.1, duration: 1 }}
-            className='flex  content-center self-center leading-tight text-base pt-0 pr-4'>
-            <TopNav href='/' target='_self'>
-              Home
-            </TopNav>
-          </motion.div>
-        </div>
-        <div className='h-20 py-2 flex flex-row content-center self-center grow'>
-          <div className='flex content-center self-center leading-tight text-base'>
-            <Team />
-          </div>
-          <motion.div
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.1, duration: 1 }}
-            className='flex  content-center self-center leading-tight text-base pt-0 pr-4'>
-            <TopNav href='/constructor-standings' target='_self'>
+        </NavLink>
+        <NavLink href='/constructor-standings' target='_self'>
+          <div className='h-20 py-2 flex flex-row content-center self-center grow'>
+            <div className='flex content-center self-center leading-tight text-base'>
+              <Team />
+            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.1, duration: 1 }}
+              className='flex content-center self-center leading-tight text-base pt-0 pr-4'>
               Team Standings
-            </TopNav>
-          </motion.div>
-        </div>
-        <div className='h-20 py-2 flex flex-row content-center self-center grow'>
-          <div className='flex content-center self-center leading-tight text-base'>
-            <Driver />
+            </motion.div>
           </div>
-          <motion.div
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.1, duration: 1 }}
-            className='flex  content-center self-center leading-tight text-base pt-0 pr-4'>
-            <TopNav href='/driver-standings' target='_self'>
+        </NavLink>
+        <NavLink href='/driver-standings' target='_self'>
+          <div className='h-20 py-2 flex flex-row content-center self-center grow'>
+            <div className='flex content-center self-center leading-tight text-base'>
+              <Driver />
+            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.1, duration: 1 }}
+              className='flex content-center self-center leading-tight text-base pt-0 pr-4'>
               Driver Standings
-            </TopNav>
-          </motion.div>
-        </div>
-        <div className='h-20 py-2 flex flex-row content-center self-center grow'>
-          <div className='flex content-center self-center leading-tight text-base'>
-            <Circuit />
+            </motion.div>
           </div>
-          <motion.div
-            initial={{ opacity: 0, scale: 1 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ delay: 0.1, duration: 1 }}
-            className='flex  content-center self-center leading-tight text-base pt-0 pr-1'>
-            <TopNav href='/circuit-details' target='_self'>
+        </NavLink>
+        <NavLink href='/circuit-details' target='_self'>
+          <div className='h-20 py-2 flex flex-row content-center self-center grow'>
+            <div className='flex content-center self-center leading-tight text-base'>
+              <Circuit />
+            </div>
+            <motion.div
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.1, duration: 1 }}
+              className='flex content-center self-center leading-tight text-base pt-0 pr-1'>
               Circuit Details
-            </TopNav>
-          </motion.div>
-        </div>
+            </motion.div>
+          </div>
+        </NavLink>
       </div>
       <div className='lg:order-1 w-full lg:w-4/12'>
         <div className='w-full flex justify-center lg:justify-start text-sm font-medium h-20 pt-1 pl-1'>
--- components/link/NavLink.tsx
+++ components/link/NavLink.tsx
@@ -11,7 +11,7 @@ const NavLink: React.FC<
   return (
     <Link href={href} prefetch={prefetch}>
       <a
-        className='border-transparent border border-dotted hover:border-primary-on hover:bg-black-5 focus:outline-none focus:border-secondary-focus focus:border-dotted active:outline-none active:shadow-none px-2 rounded-tl-xl rounded-br-xl '
+        className='border-transparent border border-dotted hover:border-primary-on text-primary-on hover:text-primary-hover hover:bg-black-5 focus:outline-none focus:border-secondary-focus focus:border-dotted active:outline-none active:shadow-none px-2 rounded-tl-xl rounded-br-xl '
         href={href}
         target={target}>
         {children}
