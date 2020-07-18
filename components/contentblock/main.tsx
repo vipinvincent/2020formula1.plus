ede973b8cad73962b71800cf9f59af532e4e2902
vipin <vipinvincentp13@gmail.comm>
2020-07-18 20:08:10 +0530

    Hungary gp qualifying

--- components/contentblock/main.tsx
+++ components/contentblock/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import H1 from 'components/heading/H1'
-import ActionLink from 'components/link/ActionLink'
+// import ActionLink from 'components/link/ActionLink'
 
 const Main: React.FC = () => {
   return (
