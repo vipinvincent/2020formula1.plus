a18cfffdfb0a3d341368b26b9c4ebb98fec88690
vipin <vipinvincentp13@gmail.comm>
2020-07-31 21:49:46 +0530

    video updates

--- components/contentblock/right.tsx
+++ components/contentblock/right.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import H1 from 'components/heading/H1'
-import ActionLink from 'components/link/ActionLink'
+// import ActionLink from 'components/link/ActionLink'
 
 const Right: React.FC = () => {
   return (
@@ -8,61 +8,68 @@ const Right: React.FC = () => {
       <div className='h-40 xl:h-48 w-full'>
         <img
           className='w-full h-full object-cover rounded-tl-xxl'
-          src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1595851663/contentImages/content/race-grid_xnnuqd.jpg'
-          alt='Hamilton clinches his EIGHTH Hungarian Grand Prix win, with Verstappen in second and Bothas in third'
+          src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1596212334/contentImages/content/silverstone_u3q2ho.jpg'
+          alt='Stroll leads Albon and Bottas in FP2; Verstappen quickest from Hamilton and Stroll in FP1 as Hulkenberg shines on his  RETURN'
         />
       </div>
       <div className='relative max-w-screen-xl mx-auto'>
         <div className='px-4 pt-4 pb-2 text-left'>
           <div className='text-base leading-6 font-semibold uppercase tracking-wider text-secondary-on'>
-            Analysis
+            FP1 and FP2
           </div>
-          <H1>How Triple-header races impact 2020 season</H1>
+          <H1>
+            Stroll leads Albon and Bottas in FP2; Verstappen quickest from
+            Hamilton and Stroll in FP1 as Hulkenberg shines on his 'RETURN'
+          </H1>
           <p className='mt-1 mb-1 text-base leading-normal text-gray-700'>
-            F1 is all set to begin its second triple-header of the season at
-            Silverstone, after running races on three consecutive weekends in
-            Austria and Hungary earlier this season. Due to the COVID-19
-            pandemic, the F1 season is currently planned to have 13 races
-            including multiple triple headers. This includes the season opener
-            races with two races in Austria and one in Hungary, which is now
-            followed by a week off, with a second leg to begin this week with
-            two races at Silverstone, followed by the Spanish GP.
+            Max Verstappen bags the P1 in the first practice session of the 2020
+            British Grand Prix for Red Bull, as Nico Hulkenberg returns to F1
+            for replacing Sergio Perez at Racing Point. Earlier Sergio Perez
+            tested positive for Covid-19 ahead of this race, only for the
+            German, the ex-Renault and Force India was called up at the last
+            minute to for a unlikely seat in Racing Point.
           </p>
           <p className='mt-1 mb-1 text-base leading-normal text-gray-700'>
-            After the first triple-header in F1 history in 2018, F1 consciously
-            decided that it would be better if they don't try it again. But the
-            current scenario forced the F1 to rethink this strategy and decided
-            to have multiple triple header races. But this time they have a
-            difference with two races in the triple headers will be hosted in
-            one venue which helps the teams and the officials altogether and
-            most importantly being able to bring something cheerful to people
-            around the world during this worse condition.
+            Verstappen shown blistering pace for Red Bull, setting a time 0.474s
+            quicker than Lewis Hamilton and +0.582s quicker than third-place
+            Lance Stroll in racing Point. Verstappen's time of 1m27.422s was a
+            full seven-tenths faster than his team mate's, as Alex Albon who
+            finished fourth. Hamilton had a new MGU-K fitted to his W11 this
+            weekend in a precautionary move by Mercedes, but he will not take a
+            grid penalty as he is still within his allowance for the season.
+            Verstappen and stroll set their best time with softs, but Hamilton
+            set his best time on mediums.
           </p>
           <p className='mt-1 mb-1 text-base leading-normal text-gray-700'>
-            Meanwhile McLaren team principal Andreas Seidl has alerted that
-            triple-header races should not become the new routine for Formula
-            1’s future calendars. Although Seidl welcomes the financial boost
-            they provide during the pandemic], but he insists they are not worth
-            since it take serious toll on the people who are part of F1 like
-            officials, team members, drivers, support staff etc. “We’re going
-            now into the triple-header with these two races in the UK, which is
-            obviously for the UK teams is not as bad as for some other teams,”
-            said Seidl. “At least we have the possibility to arrive at the track
-            as late as possible with only a short travel, and having a break
-            between these two Silverstone races. “Then we go into another
-            triple-header, and then at the moment I think on the schedule there
-            is another triple-header later on, which I think then is tough.
-            Given the special circumstances we are in this year, it’s something
-            we simply have to get through this year. “But at the same time, this
-            cannot be the new standard going forward also in future seasons.”
+            Ferrari’s Sebastian Vettel couldn’t participate in FP1 as his SF1000
+            car was having an intercooler issue and completed no timed laps,
+            while team mate Charles Leclerc rounded out the top-five with a
+            0.799s deficit to Verstappen. Valtteri Bottas with over a second off
+            Verstappen’s place mainly focused on long runs on the hard compound
+            tyres for Mercedes. The Renault teammates finished behind Bottas
+            with Esteban Ocon seventh, just under 0.020s faster than
+            eighth-place Daniel Ricciardo. Hulkenberg, running in an FP1 session
+            for the first time since last year, was just 0.6s behind his team
+            mate Stroll. Daniil Kvyat completes top 10 list with +1.446s behind
+            leader.
           </p>
           <p className='mt-1 mb-1 text-base leading-normal text-gray-700'>
-            In some instances, the new approach has made life a bit easier. As
-            per Tom Clarkson, there was a certain degree of the old days about
-            the new ways. The inability for excess had led to a stripped down
-            and simple version of the sport, one that many of us remembered as a
-            distant memory from our past and some had only ever heard about in
-            dinner tales or grumbles from their more experienced colleagues.
+            Racing Point’s Lance Stroll leads FP2 ahead of the Red Bull of Alex
+            Albon and the Mercedes of Valtteri Bottas. The session got disrupted
+            due to a crash for Albon as he was rounding the fast Stowe corner
+            when he lost the back end of his car only to snap his car the other
+            way and careened into the barriers, bringing out a red flag. Despite
+            Albon clambering out of the car himself, he was taken to the Medical
+            Centre after his g-force sensor was triggered, but the team later
+            confirmed he had been cleared by the medics.
+          </p>
+          <p className='mt-1 mb-1 text-base leading-normal text-gray-700'>
+            Ferrari’s Charles Leclerc finished an impressive P4, and Mercedes’
+            Lewis Hamilton, the six-time Silverstone winner completes the top 5
+            spots. McLaren’s Carlos Sainz was sixth, ahead of the impressive
+            Nico Hulkenberg, who finished the session seventh, 0.636s off FP2
+            leader Stroll. Pierre Gasly finished P8, ahead of Daniel Ricciardo
+            and Kimi Raikkonen completes top 10.
           </p>
 
           {/* <p className='mt-1 text-base leading-normal text-gray-700'>
@@ -75,7 +82,7 @@ const Right: React.FC = () => {
             for More Details...
           </p> */}
         </div>
-        <div className='flex flex-col justify-start border-t border-gray-200 mt-1 pb-2 px-2 text-sm  text-center'>
+        {/* <div className='flex flex-col justify-start border-t border-gray-200 mt-1 pb-2 px-2 text-sm  text-center'>
           <div className='w-full py-2 text-sm'>Follow this topic on -</div>
           <div className='w-full flex flex-row justify-center my-1'>
             <div className='mr-2'>
@@ -88,8 +95,8 @@ const Right: React.FC = () => {
                   alt='f1.com'
                 />
               </ActionLink>
-            </div>
-            {/* <div className='mr-2'>
+            </div> */}
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.bbc.com/sport/formula1/53377338'
                 target='_blank'>
@@ -100,18 +107,18 @@ const Right: React.FC = () => {
                 />
               </ActionLink>
             </div> */}
-            <div className='mr-2'>
-              <ActionLink
-                href='https://www.motorsport.com/f1/news/mclaren-triple-header-races-standard/4844013/'
-                target='_blank'>
-                <img
-                  className='w-auto h-6 md:h-12 object-contain rounded-tl-lg rounded-br-lg'
-                  src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1594826932/contentImages/logo/motorsport_acmgji.jpg'
-                  alt='motorsport.com'
-                />
-              </ActionLink>
-            </div>
-            {/* <div className='mr-2'>
+        {/* <div className='mr-2'>
+          <ActionLink
+            href='https://www.motorsport.com/f1/news/mclaren-triple-header-races-standard/4844013/'
+            target='_blank'>
+            <img
+              className='w-auto h-6 md:h-12 object-contain rounded-tl-lg rounded-br-lg'
+              src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1594826932/contentImages/logo/motorsport_acmgji.jpg'
+              alt='motorsport.com'
+            />
+          </ActionLink>
+        </div> */}
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.skysports.com/f1/news/12433/12026519/lewis-hamilton-proud-to-master-terrifying-styrian-gp-qualifying'
                 target='_blank'>
@@ -122,7 +129,7 @@ const Right: React.FC = () => {
                 />
               </ActionLink>
             </div> */}
-            {/* <div className='mr-2'>
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.autosport.com/f1/news/150565/red-bull-expects-to-close-gap-on-mercedes-in-hungary'
                 target='_blank'>
@@ -133,7 +140,7 @@ const Right: React.FC = () => {
                 />
               </ActionLink>
             </div> */}
-            {/* <div className='mr-2'>
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.espn.in/f1/story/_/id/29438233/mugello-host-first-f1-race-sochi-added-2020-calendar'
                 target='_blank'>
@@ -155,7 +162,7 @@ const Right: React.FC = () => {
                 />
               </ActionLink>
             </div>*/}
-            <div className='mr-2'>
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.grandprix247.com/2020/07/27/seidl-triple-headers-cant-be-the-new-standard/'
                 target='_blank'>
@@ -165,8 +172,8 @@ const Right: React.FC = () => {
                   alt='grandprix247.com'
                 />
               </ActionLink>
-            </div>
-            {/* <div className='mr-2'>
+            </div> */}
+        {/* <div className='mr-2'>
               <ActionLink
                 href='https://www.planetf1.com/news/qualifying-report-styrian-gp-2020/'
                 target='_blank'>
@@ -177,8 +184,8 @@ const Right: React.FC = () => {
                 />
               </ActionLink>
             </div> */}
-          </div>
-        </div>
+        {/* </div>
+        </div> */}
       </div>
     </div>
   )
