0ee6bc7df2de80ad7f1d7110e146debac8216d96
vipin <vipinvincentp13@gmail.comm>
2020-10-11 20:30:13 +0530

    race report updates

--- components/contentblock/race-report.tsx
+++ components/contentblock/race-report.tsx
@@ -8,7 +8,7 @@ const RaceReport: React.FC = () => {
       <div className='h-48 md:h-64 xl:h-104 w-full'>
         <img
           className='w-full h-full object-cover rounded-tl-xxl'
-          src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1601214934/contentImages/content/bottas_bw5vun.jpg'
+          src='https://res.cloudinary.com/f1plus/image/upload/q_auto/v1602428319/contentImages/content/hamilton-mick.png'
           alt='Race REPORT - EIFEL GP 2020'
         />
       </div>
@@ -18,57 +18,54 @@ const RaceReport: React.FC = () => {
             Race REPORT - EIFEL GP 2020
           </div>
           <H1>
-            Mercedes’ Valtteri Bottas wins in Sochi with Verstappen in Second
-            and Hamilton in third
+            Hamilton wins Eifel GP and takes record equalling 91st victory as
+            Ricciardo claims his first podium with Renault
           </H1>
           <p className='mt-1 mb-2 text-base leading-normal text-gray-700'>
-            Mercedes’ Valtteri Bottas claimed his second victory of 2020 at the
-            EIFEL Grand Prix, as a ten second penalty for the pole setter Lewis
-            Hamilton forced him to third. Meanwhile, Max Verstappen scored Red
-            Bull’s first ever podium at Sochi in P2. Hamilton started the race
-            with the investigation on for a pair of practice start infringements
-            on his laps to the grid. Later 5 seconds penalty for each incident
-            was handed to Hamilton during mid-race, ruling him out of contention
-            for the win. Incidentally, Hamilton will get a licence penalty point
-            for each of the two time penalties received in this race. This will
-            take his penalty tally to 10,two short of a race ban. Earlier,
-            Bottas had passed Verstappen for second at the race start, and
-            Verstappen himself plunged to fourth later recovered to third from
-            where both enjoyed a trouble free race to the chequered flag. This
-            would have been an confidence booster for Bottas, who needs to be
-            fighting for the championship with Hamilton and Verstappen too after
-            two DNFs.
+            Mercedes’s Lewis Hamilton has paralleled win record of Michael
+            Schumacher with his 91st F1 victory at Eifel GP 2020 as his pole
+            sitting team mate Valtteri Bottas had to retire his W11 due to a
+            loss of power, which is incidentally Mercedes’s first DNF of 2020.
+            As part of the immediate post-race interviews, Hamilton was
+            presented with one of Michael Schumacher’s race helmets by Mick
+            Schumacher, who congratulated him on the historic achievement. “I’m
+            so honoured. I really appreciate that,” Hamilton told Mick
+            Schumacher after being handed the red Mercedes helmet. Red Bull’s
+            Max Verstappen crossed the line in P2 with the fastest lap point on
+            his lat last towards the chequered flag. Meanwhile Daniel Ricciardo
+            secured his first podium for the Renault team, since his win for Red
+            Bull at Monaco in 2018. This also the first podium for Renault since
+            returning to the sport in 2016.
           </p>
           <p className='mt-1 mb-2 text-base leading-normal text-gray-700'>
-            Racing Point’s Sergio Perez came home with fourth behind the podium
-            positions. Renault’s Daniel Ricciardo, after a 5 second penalty had
-            enough pace in hand to maintain P5 at the flag, ahead of the Ferrari
-            of Charles Leclerc in sixth. Esteban Ocon in the second Renault, who
-            let his team mate pass in race after failing to overtake Leclerc,
-            crossed the line in P7. Home hero Daniil Kvyat impressed in his
-            AlphaTauri with P8 finish ahead of his teammate of Pierre Gasly.
-            Pierre Gasly himself had a personal moment in the race with the Red
-            Bull of Alex Albon, who came home P10, having started P15 after
-            receiving a five-place penalty for changing his gearbox.
+            Racing Point’s Sergio Perez came home in P4 after a tough battle
+            with Daniel for the final podium place. Carlos Sainz of sole
+            remaining McLaren finished P5 had a stealthily brilliant race from
+            AlphaTauri’s Pierre Gasly saw him take P6 after a late pass on
+            Ferrari’s Charles Leclerc. The second Racing Point of super sub Nico
+            Hulkenberg, who did a fine cameo job in place of Lance Stroll to
+            jump from P20 to P8. With such short notice, that he couldn't even
+            take part in practice on Saturday, joined the team few minutes
+            before the qualifying did an awesome race which earned the driver of
+            the day. Romain Grosjean of Haas secured his first points of the
+            season finished in P9, while Alfa Romeo’s Antonio Giovinazzi claimed
+            the final points paying position, just holding off the Ferrari of
+            Sebastian Vettel.
           </p>
           <p className='mt-1 mb-2 text-base leading-normal text-gray-700'>
-            Antonio Giovinazzi in Alfa Romeo crossed eleventh ahead of Kevin
-            Magnussen in Haas. Sebastian Vettel in the second Ferrari
-            disappoints again with a P13 finish who had few interesting battles
-            with two Renault cars in between the race. Behind Vettel, Alfa
-            Romeo’s Kimi Raikkonen finished P14 as he matched Rubens
-            Barrichello’s tally of 322 F1 starts. This means the Finn is set to
-            make a record his own at the upcoming Eifel Grand Prix at Germany.
-            Lando Norris in McLaren on could manage P15 as Willams’ Nicholas
-            Latifi finished in P16. Romain Grosjean in the second Haas and
-            George Russell in the second Willams rounded off the race finishers.
-            Meanwhile, McLaren’s Carlos Sainz who ripped the front right corner
-            off his car while trying to re-join the track at Turn 3, and Racing
-            Point’s Lance Stroll who was tagged by Charles Leclerc at Turn 4,
-            are the two DNFs of the race. These two events invoked the safety
-            car in the first lap which were the only incidents, otherwise a
-            trouble free race event, after the double red flagged races in
-            Italy.
+            Sebastian Vettel in the second Ferrari disappoints again with a
+            pointless finish in P11 ahead of Kimi Räikkönen of Alfa Romeo who
+            broke the record with 323rd start. Kevin Magnussen in second Hass
+            crossed the line in P13 with Nicholas Latifi in Williams in p14 and
+            Daniil Kvyat inn second Alpha Taruri rounded of the drivers who
+            completed the race. Earlier George Russell had to retire his car
+            after colliding with Kimi followed by Valtteri Bottas who retired
+            due to a power loss. Later Bottas was joined by the Renault of
+            Esteban Ocon, the Red Bull of Alex Albon, and the McLaren of Lando
+            Norris due to meachanical issues. Now all eyes turns to Renault team
+            boss Cyril Abiteboul who is getting a tattoo as Riccardo announces
+            that “It's real; It's going to happen” during the postrace
+            interviews.
           </p>
           {/* <p className='mt-1 mb-2 text-base leading-normal text-gray-700'>
             Check out the positions of Top-5 drivers at the time Hamilton
